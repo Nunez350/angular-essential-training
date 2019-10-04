534186d6e028c50542fc0d3cc21fc87ab29c6404
Roy Nunez <roynunez273@gmail.com>
2019-10-04 09:39:00 -0400

    styling a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'mw-app',
-  templateUrl: './app.component.html'
+  templateUrl: '<h1> My app </h1><p>keeping track of the media I want to watch</p>'
 
 
 })
